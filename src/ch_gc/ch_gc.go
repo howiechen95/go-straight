4e7ed87c65a4de1173919eb62b1a1d68a14b8c00
howiechen <howiechen95@gmail.com>
2021-08-25 08:52:51 +0800

    update

--- src/ch_gc/ch_gc.go
+++ src/ch_gc/ch_gc.go
@@ -13,3 +13,58 @@ func PrintStats(mem runtime.MemStats) {
 	fmt.Println("mem.NumGC:", mem.NumGC)
 	fmt.Println("-----")
 }
+
+// 使用 slice
+type data struct {
+	i, j int
+}
+
+func SliceGC() {
+	var N = 40000000
+	var structure []data
+	for i := 0; i < N; i++ {
+		value := int(i)
+		structure = append(structure, data{value, value})
+	}
+	runtime.GC()
+	_ = structure[0] // 防止垃圾回收器过早地垃圾收集结构体变量，因为未在for循环之外对其进行引用或使用
+}
+
+// 使用 pointers 操作 map
+func MapWithPointGC() {
+	var N = 40000000
+	myMap := make(map[int]*int)
+	for i := 0; i < N; i++ {
+		value := int(i)
+		myMap[value] = &value
+	}
+	runtime.GC()
+	_ = myMap[0]
+}
+
+// 不使用 pointers 操作 map
+func MapGC() {
+	var N = 40000000
+	myMap := make(map[int]int)
+	for i := 0; i < N; i++ {
+		value := int(i)
+		myMap[value] = value
+	}
+	runtime.GC()
+	_ = myMap[0]
+}
+
+// 分割 map
+func MapSplitGC() {
+	var N = 40000000
+	split := make([]map[int]int, 200)
+	for i := range split {
+		split[i] = make(map[int]int)
+	}
+	for i := 0; i < N; i++ {
+		value := int(i)
+		split[i%200][value] = value
+	}
+	runtime.GC()
+	_ = split[0][0]
+}
