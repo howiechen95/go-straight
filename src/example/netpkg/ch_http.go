97b7b975796e94315a3d860d74f166c7f06a90df
howiechen <howiechen95@gmail.com>
2021-09-04 23:04:20 +0800

    update

--- src/example/netpkg/ch_http.go
+++ src/example/netpkg/ch_http.go
@@ -1,1 +1,18 @@
 package netpkg
+
+import (
+	"fmt"
+	"net"
+)
+
+func TCPServer() error {
+	listenAddr := fmt.Sprintf("%s:%d", "127.0.0.1", 8080)
+	ln, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		return err
+	}
+	defer ln.Close()
+	conn, err := ln.Accept()
+	fmt.Println(conn.RemoteAddr().String(), "come in")
+	return nil
+}
